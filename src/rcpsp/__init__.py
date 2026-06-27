9e460e747a44204a5b7db4a576d28f0a9ed279f7
santino11magtalas-star <santino11.magtalas@gmail.com>
2026-06-27 11:27:58 +0200

    Update __init__.py
    Just to make sure cpm.py file exists.

--- src/rcpsp/__init__.py
+++ src/rcpsp/__init__.py
@@ -1,5 +1,4 @@
 from .model import Acitivity, Project, Schedule, CycleError
 from .parser import parse_sm 
 from .cpm import critical_path_method, CPMResult 
-
 __all__ =[ "Activity", "Project", "Schedule", "CycleErrorr", "parse_sm", "critical_path_method", "CPMResult",]
