e8d2e642e3fc9ecfb9bc452ec4bc26a48a96bef0
santino11magtalas-star <santino11.magtalas@gmail.com>
2026-07-05 17:51:58 +0200

    Add priority_order and simulated_annealing to exports

--- src/rcpsp/__init__.py
+++ src/rcpsp/__init__.py
@@ -2,7 +2,10 @@ from .model import Activity, Project, Schedule, CycleError
 from .parser import parse_sm
 from .cpm import critical_path_method, CPMResult
 from .sgs import serial_sgs, is_feasible
-
+from .priority import priority_order
+from .annealing import simulated_annealing
+ 
 __all__ = ["Activity", "Project", "Schedule", "CycleError",
            "parse_sm", "critical_path_method", "CPMResult",
-           "serial_sgs", "is_feasible"]
+           "serial_sgs", "is_feasible", "priority_order",
+           "simulated_annealing"]
