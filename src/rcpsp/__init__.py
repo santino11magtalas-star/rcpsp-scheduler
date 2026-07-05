297cc7dd6118b3fc5e91d141522f80be833d1e52
santino11magtalas-star <santino11.magtalas@gmail.com>
2026-07-05 15:34:26 +0200

    Add serial_sgs and is_feasible to module exports

--- src/rcpsp/__init__.py
+++ src/rcpsp/__init__.py
@@ -1,4 +1,8 @@
 from .model import Activity, Project, Schedule, CycleError
-from .parser import parse_sm 
-from .cpm import critical_path_method, CPMResult 
-__all__ =[ "Activity", "Project", "Schedule", "CycleErrorr", "parse_sm", "critical_path_method", "CPMResult",]
+from .parser import parse_sm
+from .cpm import critical_path_method, CPMResult
+from .sgs import serial_sgs, is_feasible
+
+__all__ = ["Activity", "Project", "Schedule", "CycleError",
+           "parse_sm", "critical_path_method", "CPMResult",
+           "serial_sgs", "is_feasible"]
