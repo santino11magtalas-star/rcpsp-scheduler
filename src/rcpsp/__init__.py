9f1d1c3979fa6851198f8991b391408d7d85e353
Santino Magtalas <santino11.magtalas@gmail.com>
2026-07-07 22:43:42 +0800

    add exact solver; fix tests/__init__.py; all 28 tests passing

--- src/rcpsp/__init__.py
+++ src/rcpsp/__init__.py
@@ -4,8 +4,9 @@ from .cpm import critical_path_method, CPMResult
 from .sgs import serial_sgs, is_feasible
 from .priority import priority_order
 from .annealing import simulated_annealing
- 
+from .exact import branch_and_bound
+
 __all__ = ["Activity", "Project", "Schedule", "CycleError",
            "parse_sm", "critical_path_method", "CPMResult",
            "serial_sgs", "is_feasible", "priority_order",
-           "simulated_annealing"]
+           "simulated_annealing", "branch_and_bound"]
